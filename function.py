a5b43553cb007caf453e5a89c0b4390af9346d32
Vittorio Somma <75581252+mrvittoriosomma@users.noreply.github.com>
2024-03-18 15:57:08 +0100

    Update function.py

--- function.py
+++ function.py
@@ -2,14 +2,4 @@ from playwright.async_api import expect
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 def api_login(playwright):
-    context = playwright.request.new_context()
-    data = {
-        'login': 'test@test.ru',
-        'password': 'test',
-        'rememberMe': 1}
-    response = context.post("https://freedom24.com/api/check-login-password", data=data)
-    assert response.status is 200
-    assert response.ok
-    print(response.status)
-    print(response.body)
-
+    pass
