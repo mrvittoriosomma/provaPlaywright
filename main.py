7175764f5e331acb4aa7e0aca4cc24d4c4a3605d
Vittorio Somma <75581252+mrvittoriosomma@users.noreply.github.com>
2024-03-18 15:48:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pytest
 from playwright.sync_api import Page
 from function import *
 
-def test_contattami(page: Page):
-    richiamami("3330000000", page)
-    print("Richiamami OK")
+def test_api_login(playwright: Playwright):
+    api_login(playwright)
 
